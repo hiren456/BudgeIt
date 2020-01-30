430bd44ec0d573a2e2f2a98e2cc3fdad9d66acd4
Judah Zammit <zammitj3@myumanitoba.ca>
2020-01-30 11:33:08 -0600

    Fill in interface files with code

--- app/src/main/java/Entry.java
+++ app/src/main/java/Entry.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+// TODO: move to proper package
+
+import java.util.Date;
 
 interface Entry {
+
+    // not allowed in interface
+    //Entry(int amount,int entryID,int catID, String details, Date date);
+
+    // getters
+    int getAmount();
+    int getEntryID();
+    int getCatID();
+    String getDetails();
+    Date getDate();
+
+    // takes values and returns a modified entry
+    // if parameter is Null that indicates not to change that variable
+    // leaves original entry untouched
+    Entry modifyEntry(int amount,int entryID,int catID, String details, Date date);
+
 }
