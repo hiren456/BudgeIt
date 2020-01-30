8216879415aca04d30cc0edc7143da4fd381ed16
Judah Zammit <zammitj3@myumanitoba.ca>
2020-01-30 12:09:48 -0600

    Add subInterfaces for the logic layer
    Added interfaces that will allow the main logic layer
    to have classes that can execute neccessary caluclations.

---
+++ app/src/main/java/LogicLayer/EntryCalculator.java
@@ -0,0 +1,11 @@
+package LogicLayer;
+
+import java.util.List;
+
+interface EntryCalculator {
+
+    // Takes a list of entrys and sums
+    // their amount
+    int sumEntryList(List<Entry> entryList);
+
+}
