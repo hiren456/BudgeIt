770e5249124472d874a3f4cbbc269d9b09ba3a1a
Judah Zammit <zammitj3@myumanitoba.ca>
2020-01-31 11:28:05 -0600

    Add stub database

--- app/src/main/java/Database/StubDatabase.java
+++ app/src/main/java/Database/StubDatabase.java
@@ -1,43 +1,121 @@
 package Database;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import Entry.Entry;
 
+
 class StubDatabase implements Database {
+
+    HashMap<Integer,Entry> entryMap;
+    int catCounter;
+    int idCounter;
+    StubDatabase(int initialIDCounter, int initialCatCounter){
+        this.entryMap = new HashMap<Integer,Entry>();
+        this.catCounter = initialCatCounter;
+        this.idCounter = initialIDCounter;
+    }
     @Override
     public void insertEntry(Entry entry) {
+        // TODO: Ensure proper entry
+        // TODO: Make sure entry with same id is not in list
+        if(entryMap.containsKey(entry.getAmount())){
 
+            //exception
+        }else{
+
+            this.entryMap.put(entry.getAmount(),entry);
+        }
+
+        // TODO: test to make sure entry is in the list
     }
 
     @Override
     public Entry selectByID(int ID) {
-        return null;
+        return this.entryMap.get(ID);
+        // TODO: test to make sure the proper entry is returned
     }
 
     @Override
     public List<Entry> selectByDate(Date startDate, Date endDate) {
-        return null;
+        // TODO: ensure valid parameters
+        ArrayList<Entry> returnList = new ArrayList<Entry>();
+
+        // find all entries within the specified range
+        for ( Entry entry : this.entryMap.values()){
+            Date date = entry.getDate();
+
+            // might be a bug if this does not include start date
+            if(date.before(endDate) && date.after(startDate)){
+
+                returnList.add(entry);
+            }
+        }
+
+        // sort the entries by date
+        Collections.sort(returnList,new SortByDate());
+
+        // TODO: test for proper list
+        // TODO: test for proper order
+        return returnList;
     }
 
     @Override
     public List<Entry> selectByCategory(int catID) {
-        return null;
+        // TODO: ensure valid parameters
+        ArrayList<Entry> returnList = new ArrayList<Entry>();
+
+        // find all entries within the specified range
+        for ( Entry entry : this.entryMap.values()){
+            int entryCatID = entry.getCatID();
+
+            // might be a bug if this does not include start date
+            if(entryCatID == catID){
+
+                returnList.add(entry);
+            }
+        }
+
+
+        // sort the entries by date
+        Collections.sort(returnList,new SortByDate());
+
+        // TODO: test for proper list
+        // TODO: test for proper order
+        return returnList;
     }
 
     @Override
     public boolean deleteEntry(int ID) {
-        return false;
+        Entry removed = this.entryMap.remove(ID);
+
+        if(removed == null){
+
+            return false;
+        }else{
+
+            return true;
+        }
     }
 
     @Override
     public int getIDCounter() {
-        return 0;
+        return this.idCounter;
     }
 
     @Override
     public void updateIDCounter(int newCounter) {
+        this.idCounter = newCounter;
+    }
+}
 
+class SortByDate implements Comparator<Entry> {
+    public int compare(Entry a, Entry b) {
+        return a.getDate().compareTo(b.getDate());
     }
 }
