a65a3bbbaf8a6ac7bc35d252e53d7412c2e5c89a
Judah Zammit <zammitj3@myumanitoba.ca>
2020-02-02 08:11:17 -0600

    Restructure test packages

---
+++ app/src/test/java/com/codemonkeys9/budgeit/LogicLayer/EntryCreator/EntryCreatorTest.java
@@ -0,0 +1,4 @@
+package com.codemonkeys9.budgeit.LogicLayer.EntryCreator;
+
+public class EntryCreatorTest {
+}
---
+++ app/src/test/java/com/codemonkeys9/budgeit/LogicLayer/EntryFetcher/EntryFetcherTest.java
@@ -0,0 +1,4 @@
+package com.codemonkeys9.budgeit.LogicLayer.EntryFetcher;
+
+public class EntryFetcherTest {
+}
