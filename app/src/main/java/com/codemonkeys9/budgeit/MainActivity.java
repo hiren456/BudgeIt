43d43b8869bdb36534af8021070d37d65c658852
Conner Kulbaski <connerkulbaski@gmail.com>
2020-02-04 13:34:46 -0600

    New Entry button creates a new entry with logic layer interface
    and also stores it in the database with logic layer. Further changes include
    app icon, logo on the toolbar.

--- app/src/main/java/com/codemonkeys9/budgeit/MainActivity.java
+++ app/src/main/java/com/codemonkeys9/budgeit/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setLogo(R.drawable.budgeit_logo);
         setSupportActionBar(toolbar);
 
         Button newEntryButton = findViewById(R.id.newEntryButton);
