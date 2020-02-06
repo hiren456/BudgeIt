3b81426d67482577461647caa43a26c4aa28d756
Zach Wolfe <wolfez@myumanitoba.ca>
2020-02-05 21:03:36 -0600

    Add convenience methods to EntryTypeVisibility

--- app/src/main/java/com/codemonkeys9/budgeit/EntryTypeVisibility.java
+++ app/src/main/java/com/codemonkeys9/budgeit/EntryTypeVisibility.java
@@ -3,5 +3,43 @@ package com.codemonkeys9.budgeit;
 enum EntryTypeVisibility {
     Income,
     Expenses,
-    Both,
+    Both;
+
+    // Shows or hides income
+    public EntryTypeVisibility toggleIncome() {
+        switch(this) {
+            case Income:
+                throw new IllegalStateException("Can't hide everything!");
+            case Expenses:
+                return Both;
+            case Both:
+                return Expenses;
+        }
+        // This return is here because Java is too dumb to realize I already handled all the cases
+        // and this code can't ever be executed:
+        return null;
+    }
+
+    // Shows or hides expenses
+    public EntryTypeVisibility toggleExpenses() {
+        switch(this) {
+            case Expenses:
+                throw new IllegalStateException("Can't hide everything!");
+            case Income:
+                return Both;
+            case Both:
+                return Income;
+        }
+        // This return is here because Java is too dumb to realize I already handled all the cases
+        // and this code can't ever be executed:
+        return null;
+    }
+
+    public boolean isIncomeVisible() {
+        return this == Income || this == Both;
+    }
+
+    public boolean areExpensesVisible() {
+        return this == Expenses || this == Both;
+    }
 }
