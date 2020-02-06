0562906846e2b3c4787118d4159c6ab753088b8e
Zach Wolfe <wolfez@myumanitoba.ca>
2020-02-05 21:34:54 -0600

    Add menu items for toggling entry visibility

--- app/src/main/java/com/codemonkeys9/budgeit/MainActivity.java
+++ app/src/main/java/com/codemonkeys9/budgeit/MainActivity.java
@@ -99,8 +99,13 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_toggle_income) {
+            visibility = visibility.toggleIncome();
+        } else if (id == R.id.action_toggle_expenses) {
+            visibility = visibility.toggleExpenses();
+        }
+        if(id == R.id.action_toggle_income || id == R.id.action_toggle_expenses) {
+            refreshTimeline();
             return true;
         }
 
