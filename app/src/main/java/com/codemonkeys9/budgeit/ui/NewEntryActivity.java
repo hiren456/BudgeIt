fa150c0ad7eef45cc2d6315d2b0a1e4d92ad69e1
Zach Wolfe <wolfez@myumanitoba.ca>
2020-02-21 08:58:41 -0600

    Replace entry type toggle with SegmentedControl

--- app/src/main/java/com/codemonkeys9/budgeit/ui/NewEntryActivity.java
+++ app/src/main/java/com/codemonkeys9/budgeit/ui/NewEntryActivity.java
@@ -2,18 +2,23 @@ package com.codemonkeys9.budgeit.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import segmented_control.widget.custom.android.com.segmentedcontrol.SegmentedControl;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ToggleButton;
 
 import com.codemonkeys9.budgeit.logiclayer.LogicLayer;
 import com.codemonkeys9.budgeit.R;
 
 public class NewEntryActivity extends AppCompatActivity {
+    // See res/values/strings.xml => "entry_types"
+    private final static int
+        INCOME = 0,
+        EXPENSE = 1;
 
+    SegmentedControl entryTypeControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,9 @@ public class NewEntryActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        this.entryTypeControl = findViewById(R.id.control_incomeOrExpense);
+        this.entryTypeControl.setSelectedSegment(0);
     }
 
     public void submitEntry(){
@@ -37,6 +45,12 @@ public class NewEntryActivity extends AppCompatActivity {
         String amount = ((EditText)findViewById(R.id.editText_amount)).getText().toString();
         String date = ((EditText)findViewById(R.id.editText_date)).getText().toString();
         String details = ((EditText)findViewById(R.id.editText_details)).getText().toString();
+
+        SegmentedControl entryTypeControl = findViewById(R.id.control_incomeOrExpense);
+        int selected = entryTypeControl.getLastSelectedAbsolutePosition();
+        if(selected == EXPENSE) {
+            amount = "-" + amount;
+        }
 
         // Hiren is working on creating exceptions
         // once that is done then createEntry will throw
@@ -47,17 +61,7 @@ public class NewEntryActivity extends AppCompatActivity {
         // "OldDateException", and "InvalidAmountFormat"
         // then you can check for each type and react/inform the user accordingly
         try {
-
-            ToggleButton tb = findViewById(R.id.button_incomeOrExpense);
-
-            if ( tb.isChecked() ) {
-                ll.createEntry("-"+amount,details,date);
-            }
-
-            else {
-                ll.createEntry(amount,details,date);
-            }
-
+            ll.createEntry(amount, details, date);
         } catch(Exception e){
             //System.out.println(e.getClass()+" bad date format");
         }
