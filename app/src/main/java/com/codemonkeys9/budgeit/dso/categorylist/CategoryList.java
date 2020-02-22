fe3da36ec18d17097f8b81e87945167d1fba2b76
Judah Zammit <zammitj3@myumanitoba.ca>
2020-02-22 16:40:11 -0600

    Add CategoryList

---
+++ app/src/main/java/com/codemonkeys9/budgeit/dso/categorylist/CategoryList.java
@@ -0,0 +1,4 @@
+package com.codemonkeys9.budgeit.dso.categorylist;
+
+public interface CategoryClass {
+}
---
+++ app/src/main/java/com/codemonkeys9/budgeit/dso/categorylist/LinkedCategoryList.java
@@ -0,0 +1,4 @@
+package com.codemonkeys9.budgeit.dso.categorylist;
+
+class LinkedCategoryList implements CategoryList {
+}
