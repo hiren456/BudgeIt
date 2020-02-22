aca71fc1d28c3a68a7329ce2fef7a3db05b8d094
Conner Kulbaski <connerkulbaski@gmail.com>
2020-02-22 15:37:02 -0600

    fixed "filter by date" crash
    app no longer crashes after entering invalid date strings on the "filter by date" activity. bug solved

--- app/src/main/java/com/codemonkeys9/budgeit/ui/DateRangeActivity.java
+++ app/src/main/java/com/codemonkeys9/budgeit/ui/DateRangeActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.codemonkeys9.budgeit.R;
+import com.codemonkeys9.budgeit.dso.date.Date;
+import com.codemonkeys9.budgeit.dso.date.DateFactory;
+import com.codemonkeys9.budgeit.exceptions.InvalidDateException;
 
 public class DateRangeActivity extends AppCompatActivity {
     @Override
@@ -18,14 +22,34 @@ public class DateRangeActivity extends AppCompatActivity {
 
         final TextView startDate = findViewById(R.id.editText_start_date);
         final TextView endDate = findViewById(R.id.editText_end_date);
+
+
         Button submitButton = findViewById(R.id.button_submit);
+
+
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent data = new Intent();
+
+                // creates two dummy dates so that the exception can be handled here rather than in the MainActivity.
+                Date start = null;
+                Date end = null;
+                try{
+                    start = DateFactory.fromString(startDate.getText().toString());
+                    end = DateFactory.fromString(endDate.getText().toString());
+                }
+                catch (InvalidDateException ide){
+                    Toast.makeText(getApplicationContext(), "Error: "+ide.getUserErrorMessage(), Toast.LENGTH_LONG).show();
+                    setResult(RESULT_CANCELED, data);
+                    System.out.println(ide.getUserErrorMessage());
+                    finish();
+                }
+
                 data.putExtra("start_date", startDate.getText().toString());
                 data.putExtra("end_date", endDate.getText().toString());
+
                 setResult(RESULT_OK, data);
                 finish();
             }
