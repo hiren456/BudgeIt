e31bcb9d9c5915d2fd31ab0bcf8a9ad8c4e7b780
Judah Zammit <zammitj3@myumanitoba.ca>
2020-02-23 18:08:31 -0600

    Delete old files

--- app/src/main/java/com/codemonkeys9/budgeit/entry/EntryFactory.java
+++
@@ -1,9 +0,0 @@
-package com.codemonkeys9.budgeit.entry;
-
-import java.time.LocalDate;
-
-public class EntryFactory {
-    public static Entry createEntry(int amount, int entryID, String details, LocalDate date){
-        return new DefaultEntry(amount, entryID, details, date);
-    }
-}
