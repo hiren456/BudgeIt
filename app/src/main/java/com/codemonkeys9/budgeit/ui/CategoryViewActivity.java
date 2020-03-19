3fc26daf696b7dd03fbe9b007a961bddc78827c3
Conner Kulbaski <connerkulbaski@gmail.com>
2020-03-18 21:57:58 -0500

    Can click a category to pull up CategoryViewActivity
    When in the categories fragment, can click a category to pull up this new (empty for now) activity.

---
+++ app/src/main/java/com/codemonkeys9/budgeit/ui/CategoryViewActivity.java
@@ -0,0 +1,16 @@
+package com.codemonkeys9.budgeit.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.codemonkeys9.budgeit.R;
+
+public class CategoryViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_view);
+    }
+}
