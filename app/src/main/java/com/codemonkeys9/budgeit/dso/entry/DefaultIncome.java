060701ede943aa651cf47569e3c9921c82250f81
Zach Wolfe <wolfez@myumanitoba.ca>
2020-03-29 14:15:38 -0500

    Remove unused imports

--- app/src/main/java/com/codemonkeys9/budgeit/dso/entry/DefaultIncome.java
+++ app/src/main/java/com/codemonkeys9/budgeit/dso/entry/DefaultIncome.java
@@ -1,11 +1,8 @@
 package com.codemonkeys9.budgeit.dso.entry;
 
 import com.codemonkeys9.budgeit.dso.amount.Amount;
-import com.codemonkeys9.budgeit.dso.amount.AmountFactory;
 import com.codemonkeys9.budgeit.dso.date.Date;
-import com.codemonkeys9.budgeit.dso.date.DateFactory;
 import com.codemonkeys9.budgeit.dso.details.Details;
-import com.codemonkeys9.budgeit.dso.details.DetailsFactory;
 
 class DefaultIncome extends DefaultEntry implements Income {
     DefaultIncome(Amount amount, int entryID, Details details, Date date,int catID) {
