469b51535dae1b0e255b19954bc68d6695b19eba
Judah Zammit <zammitj3@myumanitoba.ca>
2020-04-01 17:54:47 -0500

    Modify some factories to support dependincy injection

--- app/src/main/java/com/codemonkeys9/budgeit/logiclayer/uientrymanager/UIEntryManagerFactory.java
+++ app/src/main/java/com/codemonkeys9/budgeit/logiclayer/uientrymanager/UIEntryManagerFactory.java
@@ -4,12 +4,20 @@ import com.codemonkeys9.budgeit.logiclayer.entrycreator.EntryCreatorFactory;
 import com.codemonkeys9.budgeit.logiclayer.entrycreator.EntryCreator;
 import com.codemonkeys9.budgeit.logiclayer.entryflagger.EntryFlagger;
 import com.codemonkeys9.budgeit.logiclayer.entryflagger.EntryFlaggerFactory;
+import com.codemonkeys9.budgeit.logiclayer.idmanager.IDManager;
 import com.codemonkeys9.budgeit.logiclayer.uientrycategorizer.UIEntryCategorizer;
 import com.codemonkeys9.budgeit.logiclayer.uientrycategorizer.UIEntryCategorizerFactory;
 
 public class UIEntryManagerFactory {
     public static UIEntryManager createUIEntryManager(){
         EntryCreator entryCreator = EntryCreatorFactory.createEntryCreator();
+        EntryFlagger entryFlagger = EntryFlaggerFactory.createEntryFlagger();
+        UIEntryCategorizer categorizer = UIEntryCategorizerFactory.createUIEntryCategorizer();
+        return new DefaultUIEntryManager(entryCreator,entryFlagger,categorizer);
+    }
+
+    public static UIEntryManager createUIEntryManager(IDManager idManager){
+        EntryCreator entryCreator = EntryCreatorFactory.createEntryCreator(idManager);
         EntryFlagger entryFlagger = EntryFlaggerFactory.createEntryFlagger();
         UIEntryCategorizer categorizer = UIEntryCategorizerFactory.createUIEntryCategorizer();
         return new DefaultUIEntryManager(entryCreator,entryFlagger,categorizer);
