c3a83af389505231a461ef08841ce5e4dbe368ca
Zach Wolfe <wolfez@myumanitoba.ca>
2020-04-03 19:19:58 -0500

    Add tomorrowAtMidnight method to DateSource

--- app/src/main/java/com/codemonkeys9/budgeit/logiclayer/uirecurringentrymanager/DateSource.java
+++ app/src/main/java/com/codemonkeys9/budgeit/logiclayer/uirecurringentrymanager/DateSource.java
@@ -4,4 +4,8 @@ import com.codemonkeys9.budgeit.dso.date.Date;
 
 interface DateSource {
     Date now();
+
+    // A Java date is necessary here because it is expected by a Java Timer. Also, our Date DSO
+    // doesn't include seconds or milliseconds, which makes it unsuitable for this task anyway.
+    java.util.Date tomorrowAtMidnight();
 }
--- app/src/main/java/com/codemonkeys9/budgeit/logiclayer/uirecurringentrymanager/IRLDateSource.java
+++ app/src/main/java/com/codemonkeys9/budgeit/logiclayer/uirecurringentrymanager/IRLDateSource.java
@@ -3,8 +3,20 @@ package com.codemonkeys9.budgeit.logiclayer.uirecurringentrymanager;
 import com.codemonkeys9.budgeit.dso.date.Date;
 import com.codemonkeys9.budgeit.dso.date.DateFactory;
 
+import java.util.Calendar;
+
 class IRLDateSource implements DateSource {
     public Date now() {
         return DateFactory.fromString("now");
+    }
+
+    public java.util.Date tomorrowAtMidnight() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
     }
 }
