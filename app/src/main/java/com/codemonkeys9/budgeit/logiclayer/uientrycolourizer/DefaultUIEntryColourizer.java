df619e7626030c4ec17d5af2b93c7c9b50845b1b
Zach Wolfe <wolfez@myumanitoba.ca>
2020-04-05 00:05:21 -0500

    Use Android's Color class, which I just discovered

--- app/src/main/java/com/codemonkeys9/budgeit/logiclayer/uientrycolourizer/DefaultUIEntryColourizer.java
+++ app/src/main/java/com/codemonkeys9/budgeit/logiclayer/uientrycolourizer/DefaultUIEntryColourizer.java
@@ -1,33 +1,33 @@
 package com.codemonkeys9.budgeit.logiclayer.uientrycolourizer;
 
+import android.graphics.Color;
+
 import com.codemonkeys9.budgeit.dso.entry.BaseEntry;
 import com.codemonkeys9.budgeit.dso.entry.Purchase;
 import com.codemonkeys9.budgeit.dso.entry.RecurringPurchase;
 
 class DefaultUIEntryColourizer implements UIEntryColourizer {
-    private static final int RED   = 0xFFFF0000;
-    private static final int GREEN = 0xFF00AA00;
-    private static final int BLACK = 0xFF000000;
+    private static final int GREEN = Color.argb(0xFF, 0x00, 0xAA, 00);
 
     public int getDescriptionColour(BaseEntry entry) {
         if(entry instanceof Purchase) {
             Purchase purchase = (Purchase)entry;
             if(purchase.flagged()) {
-                return RED;
+                return Color.RED;
             }
         }
         if(entry instanceof RecurringPurchase) {
             RecurringPurchase purchase = (RecurringPurchase)entry;
             if(purchase.flagged()) {
-                return RED;
+                return Color.RED;
             }
         }
-        return BLACK;
+        return Color.BLACK;
     }
 
     public int getAmountColour(BaseEntry entry) {
         if(entry instanceof Purchase || entry instanceof RecurringPurchase) {
-            return RED;
+            return Color.RED;
         } else {
             return GREEN;
         }
